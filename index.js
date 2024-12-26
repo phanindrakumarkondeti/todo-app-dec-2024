3f9c435816119fe41a30069afcb30dd75293ca4f
phanindrakumarkondeti <phani23phani@gmail.com>
2024-12-26 22:24:15 +0530

    database connection checking

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 8000;
 //ejs engine
 app.set("view engine", "ejs");
-
+app.use(express.json());
+const connectionUrl = "mongodb://localhost:27017/todoDb";
+mongoose
+  .connect(connectionUrl)
+  .then( () => { console.log("Database is connected successfully")})
+  .catch( (error) => { console.log(error.message)})
 app.get('/', (req, res) => {
     res.send("welcome to TODO-APP")
 })
